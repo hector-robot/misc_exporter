8017990b5f6d73d272ceecb65b203da7dda3136b
hector-robot <lucidzero@gmail.com>
2020-12-07 19:26:16 -0600

    Added arm clock speed

--- misc_prom_exp.py
+++ misc_prom_exp.py
@@ -25,17 +25,28 @@ def my_app(environ, start_fn):
         ### Voltage Section
         volts_arg1: str = "measure_volts"
         volts_arg2: str = "core"
-        temp_response: str
+        volts_response: str
         if platform.machine() == "x86_64":
             volts_response = subprocess.check_output(['ssh', 'ubuntu@osd01.hq.vs', "sudo " + vcgencmd_command + " " + volts_arg1 + " " + volts_arg2])
         else:
             volts_response = subprocess.check_output([vcgencmd_command, volts_arg1, volts_arg2])
         p = re.compile('volt=(.*)V')
         m = p.match(volts_response.decode(encoding='UTF-8'))
-        #volts_output = volts_response.decode(encoding='UTF-8')
         volts_output = 'core_volts ' + m.group(1)
+        ### Voltage Section
+        clock_arg1: str = "measure_clock"
+        clock_arg2: str = "arm"
+        clock_response: str
+        if platform.machine() == "x86_64":
+            clock_response = subprocess.check_output(
+                ['ssh', 'ubuntu@osd01.hq.vs', "sudo " + vcgencmd_command + " " + clock_arg1 + " " + clock_arg2])
+        else:
+            clock_response = subprocess.check_output([vcgencmd_command, clock_arg1, clock_arg2])
+        p = re.compile('frequency\(\d\d\)=(.*)$')
+        m = p.match(clock_response.decode(encoding='UTF-8'))
+        clock_output = 'arm_clock ' + m.group(1)
         ### Consolidated Output
-        string_output = temp_output + "\n" + volts_output
+        string_output = temp_output + "\n" + volts_output + "\n" + clock_output
         byte_output = string_output.encode(encoding='UTF-8')
         return [byte_output]
     elif environ['PATH_INFO'] == '/favicon.ico':
